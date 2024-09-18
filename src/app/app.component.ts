388a3bc4f6046e74789ffb6c043d383304e445f4
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-09-18 16:46:21 -0300

    add: section sobre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [NgClass],
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.css",
 })
 export class AppComponent {
-  title = 'meu-portfolio';
+  public colapsarNavbar: boolean = false;
 }
